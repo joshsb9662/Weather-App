858f14159778c9f46fd7612a18d8466941cfd803
Firstname LastnameJoshua <joshsb9662@earthlink.net>
2012-05-26 01:59:55 -0400

    Created branch for Swipe Left/Right UI.  Basic left/right swipe functionality added.

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-
+// Pages and buttons for main nav
 var pages = ['#currentConditions', '#forecast', '#alerts'],
 	page_btn = ['#current_btn', '#forecast_btn', '#alerts_btn'],
 	forecasts = ['.summary', '.details', '.hourly'],
@@ -7,6 +7,7 @@ var pages = ['#currentConditions', '#forecast', '#alerts'],
 pageTransitions(pages, page_btn);
 pageTransitions(forecasts, forecast_btn);
 
+// Main nav function
 function pageTransitions (page, btn) {
 	$.each(page, function( i, j ) {
 	 	$( btn[i] ).on('click', function(){
@@ -15,3 +16,33 @@ function pageTransitions (page, btn) {
 		});
 	});
 }
+
+// Swipe UI
+$("body").swipeleft(function(){
+
+	if( $(pages[0]).is(":visible") ) {
+		$(pages.toString()).hide();
+		$(pages[1]).show();	
+	} else if ( $(pages[1]).is(":visible") ) {
+		$(pages.toString()).hide();
+		$(pages[2]).show();
+	} else if( $(pages[2]).is(":visible") ) {
+		// Do nothing
+	}
+
+});
+
+$("body").swiperight(function(){
+
+	if( $(pages[0]).is(":visible") ) {
+		// Do nothing
+	} else if ( $(pages[1]).is(":visible") ) {
+		$(pages.toString()).hide();
+		$(pages[0]).show();
+	} else if( $(pages[2]).is(":visible") ) {
+		$(pages.toString()).hide();
+		$(pages[1]).show();
+	}
+
+});
+
